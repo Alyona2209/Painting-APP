d4495b20aa2d011bf9188f8f471dbea0d1212473
Alena <47988326+Alyona2209@users.noreply.github.com>
2019-06-06 23:18:49 +0300

    Update router.ts
    Update router for #-system

--- src/core/router/router.ts
+++ src/core/router/router.ts
@@ -4,75 +4,6 @@ import {Constructor} from "./router_interfaces";
 
 export class Router {
 
-    routes: Array<{path: string, page: Constructor<Page>}>;
-    currentPage: Page;
-    container: HTMLElement;
-    originalLocation: string;
-
-    constructor(routes: Array<{path: string, page: Constructor<Page>}>, el: HTMLElement) {
-        this.container = el;
-        this.routes = routes;
-
-        this.currentPage = null;
-    }
-
-    async start() {
-        window.addEventListener('popstate', this);
-
-        await this.handleState(location.pathname);
-    }
-
-    stop(): void {
-        window.removeEventListener('popstate', this);
-    }
-
-    async handleEvent(event: PopStateEvent) {
-
-            let route = this.routes.find(route => route.path === event.state);
-            if (route) {
-                await this.startPage(route.page);
-
-        }
-    }
-
-    async handleState(path: string) {
-        let route = this.routes.find(route => route.path === path);
-        if (route) {
-            window.history.pushState(path, null, path);
-
-            await this.startPage(route.page);
-        }
-    }
-
-    async startPage(PageConstructor: Constructor<Page>, params?: object) {
-        if (this.currentPage) {
-            await this.currentPage.destroy();
-        }
-        // @ts-ignore
-        this.currentPage = new PageConstructor();
-        await this.currentPage.load();
-        await this.currentPage.beforeRender();
-        this.container.innerHTML = await this.currentPage.render();
-        await this.currentPage.afterRender();
-        this.arrangeATags();
-    }
-
-    arrangeATags() {
-        let aTags = document.getElementsByTagName('a');
-        for (let a of aTags) {
-            a.onclick = async (e) => {
-                e.preventDefault();
-                //window.history.pushState(null, null, a.getAttribute('href'));
-                await this.handleState(a.getAttribute('href'));
-            }
-        }
-    }
-}
-
-
-
-/*export class Router {
-
     routes: Array<{path: RegExp, page: Constructor<Page>}>;
     currentPage: Page;
     container: HTMLElement;
@@ -143,4 +74,4 @@ function createPathRegExp(path: string): RegExp {
             }$`,
         'i'
     );
-}*/
+}
